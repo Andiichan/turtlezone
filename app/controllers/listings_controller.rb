f5eea577fa207cb2f9844e9945fa2bff40bc5f72
andiichan <biemecs@yahoo.com>
2018-04-06 17:16:59 +0000

    Add update action for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -18,5 +18,16 @@ class ListingsController < ApplicationController
   end
 
   def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+
+    if @listing.update(params.require(:listing).permit(:name, :description))
+      redirect_to listings_path, notice: "Listing was successfully updated!"
+    else
+      render :edit, notice: "There wasn error processing your request!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # Routes for listings
-  get '/listings', to: 'listings#index'
-  get '/listings/new', to: 'listings#new', as: 'new_listing'
-  post '/listings', to: 'listings#create'
-  get '/listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
+  # get '/listings', to: 'listings#index'
+  # get '/listings/new', to: 'listings#new', as: 'new_listing'
+  # post '/listings', to: 'listings#create'
+  # get '/listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
+  resources :listings
 end
