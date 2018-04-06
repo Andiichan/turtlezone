ca82fe09c59bb22a13095bbff2130dd007284136
andiichan <biemecs@yahoo.com>
2018-04-06 15:59:34 +0000

    Add failing tests for listings#new

--- spec/features/listings_spec.rb
+++ spec/features/listings_spec.rb
@@ -7,4 +7,19 @@ describe "Listings" do
       expect(page.status_code).to eq(200)
     end
   end
+
+  describe "#new" do
+    it "should be accessible" do
+      visit new_listing_path
+
+      expect(page.status_code).to (eq200)
+    end
+
+    it "should render a form" do
+      visit new_listing_path
+
+      expect(page).to have_field("listing[name]")
+      expect(page).to have_field("listing[description]")
+    end
+  end
 end
