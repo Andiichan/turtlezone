df75f188a98fc66be4f85fb6465e689aa8d0f97a
andiichan <biemecs@yahoo.com>
2018-04-06 16:38:49 +0000

    Add listings#create action

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,20 @@
 class ListingsController < ApplicationController
   def index
+    @listings = Listing.all
   end
 
   def new
     @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(params.require(:listing).permit(:name, :description))
+
+    if @listing.save
+      redirect_to listings_path, notice: "Listing was successfully created!"
+    else
+      render :new
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  # Routes for listings
   get '/listings', to: 'listings#index'
   get '/listings/new', to: 'listings#new', as: 'new_listing'
+  post '/listings', to: 'listings#create'
 end
